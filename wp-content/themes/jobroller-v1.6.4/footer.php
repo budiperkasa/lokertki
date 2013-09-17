7997e8bd42348bb93cc6d1c6707a1a984e2db316
budiperkasa <budi.perkasa10@gmail.com>
2013-09-17 11:11:05 +0700

    remove footer text copyright

--- wp-content/themes/jobroller-v1.6.4/footer.php
+++ wp-content/themes/jobroller-v1.6.4/footer.php
@@ -2,7 +2,7 @@
 
     	<div class="inner">
 
-			<p><?php _e('Copyright &copy;','appthemes'); ?> <?php echo date_i18n('Y'); ?> <?php bloginfo('name'); ?>. <a href="http://www.appthemes.com/themes/jobroller/" target="_blank">Job Board Software</a> | <?php _e('Powered by','appthemes'); ?> <a href="http://wordpress.org" target="_blank">WordPress</a></p>
+			<p><?php _e('Copyright &copy;','appthemes'); ?> <?php echo date_i18n('Y'); ?></p>
 
 		</div><!-- end inner -->
 
